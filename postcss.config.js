608afaaad26fce366fe0fd7877b6411e8974db7e
Richardson25 <egaliterichardson@gmail.com>
2025-04-09 20:34:07 -0700

    TodoList_React

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,12 @@
-import tailwindcss from '@tailwindcss/postcss';
+/*import tailwindcss from '@tailwindcss/postcss';
 
 export default {
   plugins: [tailwindcss],
-};
+};*/
+
+/*export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}*/
